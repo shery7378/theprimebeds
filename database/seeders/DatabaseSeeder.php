e0405f09ae117bfcd59de286df70833c7a3b9084
Muhammad-Saaim111 <saaim@quadtrum.com>
2026-07-08 03:35:50 +0500

    grantley bed set

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -51,6 +51,7 @@ class DatabaseSeeder extends Seeder
             VermontBedSeeder::class,
             BellagioBedSeeder::class,
             GrandeurBedSeeder::class,
+            GrantleyBedSeeder::class,
             AmbassadorDivanBedSeeder::class,
             AmbassadorOttomanBedSeeder::class,
             AriXBedSeeder::class,
